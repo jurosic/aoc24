c513645c445e75df3b0a6419a68c6bfa4f988b8a
Juraj <jurosik5000@gmail.com>
2024-12-09 13:31:29 +0100

    day4 refrac

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,9 +3,7 @@ extern crate regex;
 mod pt1;
 mod pt2;
 
-use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
-use regex::Regex;
+use std::io;
 
 fn main() -> io::Result<()> {
     pt1::pt1()?;
